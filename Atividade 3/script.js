e1a12c7e3c180571f356cafd9e4298be96de0f8a
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-09-26 15:52:03 -0300

    Sim

--- Atividade 3/script.js
+++ Atividade 3/script.js
@@ -1,12 +1,10 @@
 function Indice() {
     const array = [1, 2, 3, 4, 3, 5, 8];
-    const valor = parseFloat(document.getElementById('valor').value);
-    const indice = array.indexOf(valor);
+    numbers.forEach();
 
-    const resultado = document.getElementById('resultado');
-    if (indice !== -1) {
-        resultado.textContent = `O primeiro indice do valor ${valor} é ${indice}.`;
-    } else {
-        resultado.textContent = `O valor ${valor} não foi encontrado.`;
+    function myFunction(intem, index, arr){
+    arr[index] = intem * 10;
+
+    console.log(numbers)
     }
 }
